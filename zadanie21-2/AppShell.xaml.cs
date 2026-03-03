67a23e4409c430e03ce11b0af1cf18b8691f10e0
bombiarzini <kacperdragon025@gmail.com>
2026-03-03 14:10:27 +0100

    Zadanie 21.2

---
+++ zadanie21-2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace zadanie21_2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ zadanie21-2/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace zadanie21_2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Git", "Zalogowane!", "OK");
+        }
+
+
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var Email = EmailEntry.Text;
+            var Password = PasswordEntry.Text;
+
+            if (!string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Password))
+            {
+                LoginButton.IsVisible = true;
+
+            }
+            else
+            {
+                LoginButton.IsVisible = false;
+
+
+            }
+
+        }
+    }
+}
