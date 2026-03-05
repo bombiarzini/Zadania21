58912d857206d7267dd6691684965c45b34fe03d
bombiarzini <kacperdragon025@gmail.com>
2026-03-05 10:08:55 +0100

    Zadanie 21.2

---
+++ Zadanie21-3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Zadanie21_3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Zadanie21-3/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Zadanie21_3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            if(Zgoda.IsChecked == false)
+            {
+                DisplayAlert("Status", "Nie można się zarejestrować bez zgody", "Ok!");
+                return;
+            }
+            DisplayAlert("Status", "Zarejestrowano", "Ok!");
+        }
+    }
+}
